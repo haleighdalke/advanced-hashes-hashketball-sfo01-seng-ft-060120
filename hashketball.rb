704830ff411905e71d7caec7e9073fc8c70c8920
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-13 17:53:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -237,11 +237,12 @@ end
 
 
 # Which team has the most points?
-def winning_team
+# def winning_team
+# 
+# end
+# 
+# winning_team
 
-end
-
-winning_team
 # Which player has the longest name?
 def player_with_longest_name
   game_hash = game_hash()
