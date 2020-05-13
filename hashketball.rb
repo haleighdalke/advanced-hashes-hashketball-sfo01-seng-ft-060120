19cb99fcb1375437fcbb6517c06a061a3f38363f
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-13 17:26:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -233,7 +233,7 @@ def winning_team
     team.each do |e|
       e[:points] += total_points
     end
-    memo[team[team_name]] => total_points
+    memo[:team[:team_name]] => total_points
   end
 end
 
