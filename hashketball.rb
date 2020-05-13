8416c5145a7709d6edc6a9a6f1b98d53b61a6207
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-13 17:19:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -235,10 +235,14 @@ def player_with_longest_name
   longest_name = ""
   game_hash.each do |k, team|
     team[:players].each do |e|
+      binding.pry
       if e[:player_name].length > longest_name.length
         longest_name = e[:player_name]
       end
     end
   end
+  binding.pry
   longest_name
 end
+
+player_with_longest_name
