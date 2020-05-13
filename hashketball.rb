5860a1c6bfcf9c6da8a6b4987d7ca70a3c6249eb
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-13 16:51:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -135,7 +135,7 @@ def num_points_scored(player)
   game_hash.each do |k, team|
     team[:players].each do |e|
       if e[:player_name] == player
-        
+        return e[:points]
       end
     end
   end
